4357bfe1284a5c50992761c4491dabe2b0ddeca9
Marcos <marcosviniciuus0103@gmail.com>
2026-05-15 20:34:02 -0300

    atividade de nome e sobrenome

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Digite seu primeiro nome");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu sobrenome");
+ string sobrenome = Console.ReadLine();
+
+ Console.WriteLine($"nome sobrenome");
+Console.WriteLine($"{ nome } {sobrenome }");
+ 
+Console.WriteLine($"{sobrenome},{nome}");
